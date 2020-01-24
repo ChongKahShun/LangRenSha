a2f227661074a25667b8ba294b33071e69e79876
Vin <chanvs-wa16@student.tarc.edu.my>
2020-01-24 22:23:14 +0800

    https://github.com/ChongKahShun/LangRenSha.git

---
+++ Wolf/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import Navigator from "./routes/route";
+
+import firebase from "firebase";
+
+export default function App() {
+  return <Navigator />;
+}
