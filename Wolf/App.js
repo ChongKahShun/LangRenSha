642d16214471f1912a726334508580addceb85e9
ChongKahShun <melvin_chong1998@hotmail.com>
2020-02-01 22:23:32 +0800

    test

--- Wolf/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
-import Navigator from "./routes/route";
-
-import firebase from "firebase";
-
-export default function App() {
-  return <Navigator />;
-}
